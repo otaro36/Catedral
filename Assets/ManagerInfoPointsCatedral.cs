d0df3f1c3b34a01fc5320612199be0db08b6907b
otaro36 <68829944+otaro36@users.noreply.github.com>
2022-11-24 23:35:24 -0500

    Modificaciones
    This reverts commit 298dfc9b13dddbb9f8101bcddaeb6fb0c083affe.

--- Assets/ManagerInfoPointsCatedral.cs
+++ Assets/ManagerInfoPointsCatedral.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 
 public class ManagerInfoPointsCatedral : MonoBehaviour
 {
+    public string[] titlePanel;
     public string[] InfoPanel;
     public GameObject panelInfo;
+    public TMP_Text title;
     public TMP_Text text;
 
     public void ChangeInfo(int id)
     {
+        title.text=titlePanel[id];
         text.text=InfoPanel[id];
         panelInfo.SetActive(true);
     }
